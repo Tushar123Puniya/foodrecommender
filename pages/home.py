e63d886619d346685c1c4e74a0abeef7acb0226f
13anuraggupta <13.anuraggupta@gmail.com>
2024-04-15 08:03:48 +0530

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -55,6 +55,19 @@ def about_us():
 def account():
     st.title("My Account")
 
+    st.write(
+        """
+        ## About Us
+        A pioneering meal planning platform dedicated to transforming how individuals approach nutrition and wellness. Our mission is to empower our users to make informed dietary choices that align with their health goals and preferences.
+
+        At NutriAI Planner, we leverage the latest advancements in artificial intelligence to deliver personalized meal recommendations based on user-specific data like height, weight, dietary objectives, taste preferences, and dietary restrictions or allergies. This meticulous approach ensures that every meal recommendation is not only nutritionally balanced but also perfectly suited to individual needs and tastes.
+
+        We prioritize user satisfaction and strive for excellence in delivering a seamless experience. Whether you're looking to achieve weight loss, muscle gain, or simply maintain a healthy diet, NutriAI Planner is your trusted partner in achieving your nutritional aspirations.
+
+        Embark on this journey to a healthier and happier lifestyle with NutriAI Planner.
+        """
+    )
+
     
 def home():
     st.write(
